7da4fe0d2eaa3b622b84fc7906ed06f96ce3c4ed
fqararyah@gmail.com <fqararyah@gmail.com>
2023-10-29 08:37:41 +0100

    a

--- run_tuning.sh
+++ run_tuning.sh
@@ -5,18 +5,18 @@ cd /media/SSD2TB/fareed/wd/my_repos/tvm_8_bit/tvm-cuda-int8-benchmark
 OPT_ITERATION=20
 
 # Run the Python script
-python3 tune_relay_cuda.py gprox_3 $OPT_ITERATION xgb_knob
-python3 tune_relay_cuda.py gprox_3 $OPT_ITERATION random
-python3 tune_relay_cuda.py gprox_3 $OPT_ITERATION ga
+python3 tune_relay_int8_cuda.py gprox_3 $OPT_ITERATION xgb_knob
+python3 tune_relay_int8_cuda.py gprox_3 $OPT_ITERATION random
+python3 tune_relay_int8_cuda.py gprox_3 $OPT_ITERATION ga
 
-python3 tune_relay_cuda.py mob_v2 $OPT_ITERATION xgb_knob
-python3 tune_relay_cuda.py mob_v2 $OPT_ITERATION random
-python3 tune_relay_cuda.py mob_v2 $OPT_ITERATION ga
+python3 tune_relay_int8_cuda.py mob_v2 $OPT_ITERATION xgb_knob
+python3 tune_relay_int8_cuda.py mob_v2 $OPT_ITERATION random
+python3 tune_relay_int8_cuda.py mob_v2 $OPT_ITERATION ga
 
-python3 tune_relay_cuda.py mob_v1 $OPT_ITERATION xgb_knob
-python3 tune_relay_cuda.py mob_v1 $OPT_ITERATION random
-python3 tune_relay_cuda.py mob_v1 $OPT_ITERATION ga
+python3 tune_relay_int8_cuda.py mob_v1 $OPT_ITERATION xgb_knob
+python3 tune_relay_int8_cuda.py mob_v1 $OPT_ITERATION random
+python3 tune_relay_int8_cuda.py mob_v1 $OPT_ITERATION ga
 
-python3 tune_relay_cuda.py xce_r $OPT_ITERATION xgb_knob
-python3 tune_relay_cuda.py xce_r $OPT_ITERATION random
-python3 tune_relay_cuda.py xce_r $OPT_ITERATION ga
+python3 tune_relay_int8_cuda.py xce_r $OPT_ITERATION xgb_knob
+python3 tune_relay_int8_cuda.py xce_r $OPT_ITERATION random
+python3 tune_relay_int8_cuda.py xce_r $OPT_ITERATION ga
